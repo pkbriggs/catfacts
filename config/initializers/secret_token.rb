f8f587c8e15fd2f7866d0af1fb9af071249bdb2e
Patrick Briggs <patkbriggs@gmail.com>
2013-10-18 00:39:47 -0700

    Created rails project, added some dependencies

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Catfacts::Application.config.secret_key_base = 'd25d0f19626a957447d1cb67c5b7076c7daafcd056a3a509149fcdc43ca4936283ea47d046a5f22199112e1316f51027da92e818e5b992a04ed8bc643fa66704'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Catfacts::Application.config.session_store :cookie_store, key: '_catfacts_session'
